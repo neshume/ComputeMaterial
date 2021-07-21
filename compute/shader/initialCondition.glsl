3f52043a72a876e9c7ed983e2222507819ecdda3
stevejtrettel <stevejtrettel@gmail.com>
2021-07-21 16:33:19 -0700

    removed old functions

--- compute/shader/initialCondition.glsl
+++ compute/shader/initialCondition.glsl
@@ -5,20 +5,6 @@
 //before the third frame, what is happening?
 //-------------------------------------------------
 
-
-
-//this needs to be updated: its just the wavepacket at TIME ZERO
-//NEED HALF DT TIMESTEP
-vec2 wavePacket(vec2 uv, float scale, vec2 iniPos,vec2 iniMom){
-
-    vec2 relPos=scale*(uv-iniPos);
-
-    float mag=exp(-dot(relPos,relPos));
-    float phase=dot(relPos,iniMom);
-
-    return mag*vec2(cos(phase),sin(phase));
-}
-
 //
 //
 //vec2 gaussianPacket(vec2 uv, vec2 p, vec2 cent, float a, float t){
@@ -56,14 +42,8 @@ vec2 gaussianPacket(vec2 uv, vec2 p, vec2 cent, float a, float t){
     float sigma=1./a;
     float sigma2=sigma*sigma;
 
-//    vec2 cplx=vec2(1,t/(2.*sigma2));
-//    vec2 coef=sqrt(2.*PI*sigma2)*cplx;
-//    coef=inv(coef);
-//    coef=sqrtC(coef);
-//
-//
-
-    float coef=1./sqrt(sigma);
+    //rescaling to keep volume constant
+    float coef=a;
 
     //the rest of this is correct
     vec2 x = uv-cent-p*t;
@@ -71,12 +51,12 @@ vec2 gaussianPacket(vec2 uv, vec2 p, vec2 cent, float a, float t){
     float arg= dot(x,p)-dot(p,p)*t/2.;
 
     vec2 phase=vec2(cos(arg),sin(arg));
-    return 0.5*coef*mag*phase;
+    return 0.05*coef*mag*phase;
 }
 
 
 //this initial condition gives R(0) as first entry
-//and I(0.5dt) as the second entry
+//and I(dt) as the second entry
 vec2 initialCondition(ivec2 ij){
 
     vec2 uv=toUV(ij);
@@ -84,16 +64,14 @@ vec2 initialCondition(ivec2 ij){
 
     vec2 p = 20.*momentum*scale*vec2(1,0);
     vec2 cent=vec2(-0.4,0);
-    float a = 10.*(spread+0.1)*scale;
+    float width=(0.6*spread*spread+0.02);
+    float a = 50.*width*scale;
 
     //compute real and imaginary parts at different time steps
     //seems unnecessary from looking at results,but is "correct"
     vec2 psi0=gaussianPacket(uv,p,cent,a,0.);
     vec2 psi1=gaussianPacket(uv,p,cent,a,dt);
     return vec2(psi0.x,psi1.y);
-
-    //vec2 psi1=wavePacket(uv,30.,vec2(-0.3,0),20.*momentum*vec2(1,0));
-    //return psi1;
 }
 
 
